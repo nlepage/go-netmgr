b1aca92d29778a068f8f30d21c7dac065f2fe017
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2020-04-15 22:47:50 +0200

    :bulb:

--- connection_active.go
+++ connection_active.go
@@ -4,4 +4,6 @@ import (
 	"github.com/nlepage/go-netmgr/internal/dbusutil"
 )
 
+// ConnectionActive represents an attempt to connect to a network using the details provided by a Connection object.
+// See https://developer.gnome.org/NetworkManager/stable/gdbus-org.freedesktop.NetworkManager.Connection.Active.html for more information.
 type ConnectionActive dbusutil.BusObject
--- networkmanager.go
+++ networkmanager.go
@@ -20,6 +20,13 @@ func New(conn *dbus.Conn) *NetworkManager {
 	return (*NetworkManager)(dbusutil.NewBusObject(conn, networkManagerPath, networkManagerInterface, dbusutil.NewSignalManager(conn)))
 }
 
+// System returns the Connection Manager from the system bus.
+// This is equivalent to:
+//  conn, err := dbus.SystemBus()
+//  if err != nil {
+//  	return nil, err
+//  }
+//  nm := netmgr.New(conn)
 func System() (*NetworkManager, error) {
 	conn, err := dbus.SystemBus()
 	if err != nil {
@@ -29,10 +36,14 @@ func System() (*NetworkManager, error) {
 	return New(conn), nil
 }
 
+// Reload NetworkManager's configuration and perform certain updates, like flushing a cache or rewriting external state to disk.
+// See https://developer.gnome.org/NetworkManager/stable/gdbus-org.freedesktop.NetworkManager.html#gdbus-method-org-freedesktop-NetworkManager.Reload for more information.
 func (nm *NetworkManager) Reload(flags uint) error {
 	return (*dbusutil.BusObject)(nm).Call("Reload", nil, flags)
 }
 
+// Reload NetworkManager's configuration and perform certain updates, like flushing a cache or rewriting external state to disk.
+// See https://developer.gnome.org/NetworkManager/stable/gdbus-org.freedesktop.NetworkManager.html#gdbus-method-org-freedesktop-NetworkManager.Reload for more information.
 func Reload(flags uint) error {
 	nm, err := System()
 	if err != nil {
@@ -41,10 +52,14 @@ func Reload(flags uint) error {
 	return nm.Reload(flags)
 }
 
+// GetDevices gets the list of realized network devices.
+// See https://developer.gnome.org/NetworkManager/stable/gdbus-org.freedesktop.NetworkManager.html#gdbus-method-org-freedesktop-NetworkManager.GetDevices for more information.
 func (nm *NetworkManager) GetDevices() ([]*Device, error) {
 	return nm.getDevices("GetDevices")
 }
 
+// GetDevices gets the list of realized network devices.
+// See https://developer.gnome.org/NetworkManager/stable/gdbus-org.freedesktop.NetworkManager.html#gdbus-method-org-freedesktop-NetworkManager.GetDevices for more information.
 func GetDevices() ([]*Device, error) {
 	nm, err := System()
 	if err != nil {
@@ -53,10 +68,14 @@ func GetDevices() ([]*Device, error) {
 	return nm.GetDevices()
 }
 
+// GetAllDevices gets the list of all network devices.
+// See https://developer.gnome.org/NetworkManager/stable/gdbus-org.freedesktop.NetworkManager.html#gdbus-method-org-freedesktop-NetworkManager.GetAllDevices for more information.
 func (nm *NetworkManager) GetAllDevices() ([]*Device, error) {
 	return nm.getDevices("GetAllDevices")
 }
 
+// GetAllDevices gets the list of all network devices.
+// See https://developer.gnome.org/NetworkManager/stable/gdbus-org.freedesktop.NetworkManager.html#gdbus-method-org-freedesktop-NetworkManager.GetAllDevices for more information.
 func GetAllDevices() ([]*Device, error) {
 	nm, err := System()
 	if err != nil {
@@ -79,7 +98,9 @@ func (nm *NetworkManager) getDevices(method string) ([]*Device, error) {
 	return devices, nil
 }
 
-func (nm *NetworkManager) GetDeviceByIpIface(iface string) (*Device, error) {
+// GetDeviceByIPIface returns the object path of the network device referenced by its IP interface name.
+// See https://developer.gnome.org/NetworkManager/stable/gdbus-org.freedesktop.NetworkManager.html#gdbus-method-org-freedesktop-NetworkManager.GetDeviceByIpIface for more information.
+func (nm *NetworkManager) GetDeviceByIPIface(iface string) (*Device, error) {
 	var path dbus.ObjectPath
 	if err := (*dbusutil.BusObject)(nm).Call("GetDeviceByIpIface", &path, iface); err != nil {
 		return nil, err
@@ -87,10 +108,22 @@ func (nm *NetworkManager) GetDeviceByIpIface(iface string) (*Device, error) {
 	return nm.device(path), nil
 }
 
+// GetDeviceByIPIface returns the object path of the network device referenced by its IP interface name.
+// See https://developer.gnome.org/NetworkManager/stable/gdbus-org.freedesktop.NetworkManager.html#gdbus-method-org-freedesktop-NetworkManager.GetDeviceByIpIface for more information.
+func GetDeviceByIPIface(iface string) (*Device, error) {
+	nm, err := System()
+	if err != nil {
+		return nil, err
+	}
+	return nm.GetDeviceByIPIface(iface)
+}
+
 func (nm *NetworkManager) device(path dbus.ObjectPath) *Device {
 	return (*Device)((*dbusutil.BusObject)(nm).NewBusObject(path, deviceIface))
 }
 
+// ActivateConnection activates a connection using the supplied device.
+// See https://developer.gnome.org/NetworkManager/stable/gdbus-org.freedesktop.NetworkManager.html#gdbus-method-org-freedesktop-NetworkManager.ActivateConnection for more information.
 func (nm *NetworkManager) ActivateConnection(connection interface{}, device interface{}, specificObject interface{}) (interface{}, error) {
 	var args = make([]interface{}, 3)
 	var err error
